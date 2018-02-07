e8705b52696b70cd95e285dc1d13405bcf3374ab
fiordi.stefano99 <fiordi.stefano99@gmail.com>
2018-02-07 11:49:16 -0500

    Added reload buttons

--- indovina2.php
+++ indovina2.php
@@ -8,37 +8,49 @@
   </head>
   <body>
     <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-      $numero = test_input($_POST["numero"]);
-      if($_SESSION["n"] > $numero)
+    if ($_SERVER["REQUEST_METHOD"] == "POST") 
+    {
+      if(isset($_POST["reload"]))
       {
-        $res = "<h3>Il numero è troppo piccolo</h3>";
-        $_SESSION["count"]++;
-      }
-      else if($_SESSION["n"] < $numero)
-      {
-        $res = "<h3>Il numero è troppo grande</h3>";
-        $_SESSION["count"]++;
+        session_unset();
+        session_destroy();
+        header("Refresh:0");
       }
       else
       {
-        $_SESSION["vis"] = "none";
-        $res = "BRAVO!<br>Hai indovinato in ". $_SESSION["count"] ." tentativi.<br><br>";
-      }
-      if($_SESSION["count"] > 7)
-      {
-        $_SESSION["vis"] = "none";
-        $res = "Spiacenti...<br>hai superato il massimo di 7 tentativi.<br><br>";
+        $numero = test_input($_POST["numero"]);
+        if($_SESSION["n"] > $numero)
+        {
+          $res = "<h3>Il numero è troppo piccolo</h3>";
+          $_SESSION["count"]++;
+        }
+        else if($_SESSION["n"] < $numero)
+        {
+          $res = "<h3>Il numero è troppo grande</h3>";
+          $_SESSION["count"]++;
+        }
+        else
+        {
+          $_SESSION["vis"] = "none";
+          $res = "BRAVO!<br>Hai indovinato in ". $_SESSION["count"] ." tentativi.<br><br><form action=\"\" method=\"post\"><input type=\"submit\" name=\"reload\" value=\"Gioca di nuovo\"></form>";
+        }
+        if($_SESSION["count"] > 7)
+        {
+          $_SESSION["vis"] = "none";
+          $res = "Spiacenti...<br>hai superato il massimo di 7 tentativi.<br><br><form action=\"\" method=\"post\"><input type=\"submit\" name=\"reload\" value=\"Gioca di nuovo\"></form>";
+        }
       }
     }
-    else {
+    else 
+    {
       $_SESSION["vis"] = "block"; 
       $_SESSION["count"] = 1;
       $_SESSION["n"] = rand(1, 100);
       $res="";
     } 
       
-    function test_input($data) {
+    function test_input($data) 
+    {
       $data = trim($data);
       $data = stripslashes($data);
       $data = htmlspecialchars($data);
@@ -49,7 +61,7 @@
       <h2>
         Gioco dell'indovina numero
       </h2>
-      <?php echo $res;?>
+      <?php if(isset($res)){ echo $res; }?>
       <div style="display: <?php echo $_SESSION["vis"]; ?>;">
         <p>Tentativo n.<?php echo $_SESSION["count"]; ?>
           <br>
